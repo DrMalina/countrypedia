678f2b018d1d0e6dd59a90e253560b0d4f0094fc
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-08 16:15:45 +0200

    Create themes used for light and dark mode

---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+export const lightMode = {
+  primary: {
+    light: '#ffffff',
+    main: '#fafafa',
+    dark: '#c7c7c7',
+  },
+  secondary: {
+    light: '#ff4081',
+    main: '#f50057',
+    dark: '#c51162',
+  },
+};
+
+export const darkMode = {
+  primary: {
+    light: '#62727b',
+    main: '#37474f',
+    dark: '#102027',
+  },
+  secondary: {
+    light: '#ff8a50',
+    main: '#ff5722',
+    dark: '#c41c00',
+  },
+};
--- src/types/createPalette.d.ts
+++
@@ -1,8 +0,0 @@
-import * as createPalette from '@material-ui/core/styles/createPalette';
-
-declare module '@material-ui/core/styles/createPalette' {
-  interface PaletteOptions {
-    typography: PaletteColorOptions;
-    input: PaletteColorOptions;
-  }
-}
