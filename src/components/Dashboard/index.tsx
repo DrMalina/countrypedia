d46e013e5466d937fef696c4fe349164e943d5e8
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-10 20:25:11 +0200

    Break down Dashboard into smaller components and add custom hook to fetching data

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,13 +1,11 @@
 import React, { FC } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import FormControl from '@material-ui/core/FormControl';
-import InputBase from '@material-ui/core/InputBase';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Paper from '@material-ui/core/Paper';
-import SearchIcon from '@material-ui/icons/Search';
-import Select from '@material-ui/core/Select';
+import { useDataApi } from '../../hooks';
+import { Search } from '../Search';
+import { RegionFilter } from '../RegionFilter';
+
+const BASE_URL = 'https://restcountries.eu/rest/v2/all';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,26 +13,6 @@ const useStyles = makeStyles((theme: Theme) =>
       marginTop: theme.spacing(10),
       display: 'flex',
       justifyContent: 'space-between',
-    },
-    search: {
-      position: 'relative',
-      marginLeft: 0,
-      width: '20rem',
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      padding: theme.spacing(1.6, 1, 1, 0),
-      borderRadius: theme.shape.borderRadius,
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      width: '100%',
     },
     filter: {
       width: '12rem',
@@ -46,30 +24,14 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 export const Dashboard: FC = () => {
+  const [{ data, isLoading, isError }, doFetch] = useDataApi('');
+
   const classes = useStyles();
 
   return (
     <Container maxWidth="lg" className={classes.root}>
-      <Paper className={classes.search} elevation={2}>
-        <div className={classes.searchIcon}>
-          <SearchIcon />
-        </div>
-        <InputBase
-          placeholder="Search for a country..."
-          className={classes.inputRoot}
-          inputProps={{ 'aria-label': 'search' }}
-        />
-      </Paper>
-      <div className={classes.filter}>
-        <FormControl className={classes.select}>
-          <InputLabel>Choose a region</InputLabel>
-          <Select labelId="demo-simple-select-label">
-            <MenuItem value={10}>Africa</MenuItem>
-            <MenuItem value={20}>North America</MenuItem>
-            <MenuItem value={30}>Europe</MenuItem>
-          </Select>
-        </FormControl>
-      </div>
+      <Search />
+      <RegionFilter />
     </Container>
   );
 };
