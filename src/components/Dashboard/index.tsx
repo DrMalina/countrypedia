efb86c8e785398bcc24416d2a2c607c4bd6dce19
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-11 14:39:31 +0200

    Fetch regions in a parent component and pass them as props to avoid additional api calls

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,37 +1,46 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { useDataApi } from '../../hooks';
+import { CardsGrid } from '../CardsGrid';
 import { Search } from '../Search';
 import { RegionFilter } from '../RegionFilter';
-
-const BASE_URL = 'https://restcountries.eu/rest/v2/all';
+import { useDataApi } from '../../hooks';
+import { filterUniqueValues } from '../../utils';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       marginTop: theme.spacing(10),
+    },
+    filters: {
       display: 'flex',
       justifyContent: 'space-between',
-    },
-    filter: {
-      width: '12rem',
-    },
-    select: {
-      width: '100%',
     },
   }),
 );
 
+const ALL_COUNTRIES_URL = 'https://restcountries.eu/rest/v2/all';
+
 export const Dashboard: FC = () => {
   const [{ data, isLoading, isError }, doFetch] = useDataApi('');
+  const [regions, setRegions] = useState<string[]>(['All', 'Oceania']);
+
+  useEffect(() => {
+    // populate regions select only once on first api call
+    if (data.hits.length !== 0 && regions.length === 0) {
+      setRegions(['All', ...filterUniqueValues(data.hits, 'region').sort()]);
+    }
+  }, [data, regions]);
 
   const classes = useStyles();
 
   return (
     <Container maxWidth="lg" className={classes.root}>
-      <Search />
-      <RegionFilter />
+      <div className={classes.filters}>
+        <Search />
+        <RegionFilter regions={regions} />
+      </div>
+      <CardsGrid />
     </Container>
   );
 };
