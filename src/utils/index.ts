1b377140e4cd80006b3d46110a655937c5d73c66
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-12 21:33:41 +0200

    Add util fns for card properties

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,15 @@ type Data = {
 export const filterUniqueValues = (data: Data[], searchedKey: string): string[] => {
   return [...new Set(data.map((element) => element[searchedKey]))].filter(Boolean);
 };
+
+export const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+type Value = string | number;
+
+export const convertValueToLocale = (element: Value): Value => {
+  if (!element) return '-';
+
+  return isNaN(+element) ? element : element.toLocaleString();
+};
