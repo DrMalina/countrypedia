2c2c193a4a689874bf249bf16e754ee04e960deb
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-14 16:53:10 +0200

    Add util for checking if query is empty

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,3 @@
-type Data = {
-  [key: string]: string;
-};
-
-// return unique values for key in array of objects
-export const filterUniqueValues = (data: Data[], searchedKey: string): string[] => {
-  return [...new Set(data.map((element) => element[searchedKey]))].filter(Boolean);
-};
-
 export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
@@ -17,4 +8,8 @@ export const convertValueToLocale = (element: Value): Value => {
   if (!element) return '-';
 
   return isNaN(+element) ? element : element.toLocaleString();
+};
+
+export const checkIfEmpty = (str: string): Boolean => {
+  return str.length === 0 || !str.trim();
 };
