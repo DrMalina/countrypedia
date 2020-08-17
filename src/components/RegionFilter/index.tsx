e7e70ab20365b98a21df937dce308fcf01173ab2
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-17 20:24:19 +0200

    Change default value to empty string in MUI select component as suggested in docs

--- src/components/RegionFilter/index.tsx
+++ src/components/RegionFilter/index.tsx
@@ -23,7 +23,7 @@ const useStyles = makeStyles(() =>
 );
 
 export const RegionFilter: FC<RegionFilterProps> = ({
-  current = 'All',
+  current = '',
   regions = [],
   handleRegionChange,
 }) => {
