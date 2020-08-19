91e8d6875cc4e926d543a8db7dd1220e056685a0
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-19 17:52:54 +0200

    Import shared  error interface to useDataApi

--- src/hooks/useDataApi.ts
+++ src/hooks/useDataApi.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Error } from '../types/';
+import { Error } from 'types';
 
 type Query = string;
 type Loading = boolean;
