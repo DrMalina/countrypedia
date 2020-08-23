499130bc38821e157b5eefa2b7325d50d7bd0dd9
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-23 16:46:52 +0200

    Expand shared types to cover api responses

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,32 @@
+export interface Currency {
+  code: string;
+  name: string;
+  symbol: string;
+}
+
+export interface Language {
+  name: string;
+  nativeName: string;
+}
+
 export interface Country {
   name: string;
   flag: string;
   population: number;
   region: string;
+  subregion: string;
   capital: string;
+  nativeName: string;
+  topLevelDomain: string[];
+  currencies: Currency[];
+  languages: Language[];
+  borders: string[];
+  alpha3Code: string;
+}
+
+export interface Neighbor {
+  slug: string;
+  name: string;
 }
 
 export interface Error {
