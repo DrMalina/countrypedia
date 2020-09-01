a9604a4400ac3dd9853c13e1e957de5ee108e713
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-09-01 15:21:28 +0200

    Find neighbors full names using country codes from  context

--- src/views/CountryPage.tsx
+++ src/views/CountryPage.tsx
@@ -1,9 +1,10 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useCallback } from 'react';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 import slugify from 'slugify';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import Fade from '@material-ui/core/Fade';
 import Grid from '@material-ui/core/Grid';
 import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 import Typography from '@material-ui/core/Typography';
@@ -57,97 +58,99 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const CountryPage: FC = () => {
   const location = useLocation();
-  const { countries } = useCountries();
+  const { countries, countryCodes } = useCountries();
   const [country, setCountry] = useState<Country | null>(null);
   const [neighbors, setNeighbors] = useState<Neighbor[]>([]);
   const classes = useStyles();
 
-  const findNeighbor = (alphaCode: string) => {
-    const neighbor = countries.find((country) => country.alpha3Code === alphaCode);
+  const findNeighbor = useCallback(
+    // neighbors in API response are saved as alphaCodes ('GER' => 'Germany')
+    (alphaCode: string) => {
+      //const neighbor = countries.find((country) => country.alpha3Code === alphaCode);
+      const neighborName = countryCodes[alphaCode];
 
-    if (neighbor) {
-      setNeighbors((prevState) => [
-        ...prevState,
-        { name: neighbor.name, slug: slugify(neighbor.name, { lower: true }) },
-      ]);
-    }
-  };
+      if (neighborName) {
+        setNeighbors((prevState) => [
+          ...prevState,
+          { name: neighborName, slug: slugify(neighborName, { lower: true }) },
+        ]);
+      }
+    },
+    [countryCodes],
+  );
 
   // on location change, find country matching new url
   useEffect(() => {
-    //reset scroll
     window.scrollTo(0, 0);
     setNeighbors([]);
+    setCountry(null);
+
     const result = countries.find((country) => country.name === location.state);
 
     if (result) {
       setCountry(result);
+      //when there is a country (=result), find its neighbors
+      result.borders.forEach((alphaCode) => findNeighbor(alphaCode));
     }
-  }, [location]);
-
-  // when country changes, find its new neighbors
-  useEffect(() => {
-    // each neighbor (border country) is saved as alpha3Code
-    if (country) {
-      country.borders.forEach((alphaCode) => findNeighbor(alphaCode));
-    }
-  }, [country]);
+  }, [location, countries, findNeighbor]);
 
   return (
-    <Container maxWidth="lg" className={classes.root}>
-      <Button
-        variant="contained"
-        color="secondary"
-        startIcon={<KeyboardBackspaceIcon />}
-        component={RouterLink}
-        to="/"
-      >
-        Back
-      </Button>
-      {country ? (
-        <Grid container className={classes.wrapper} alignItems="center">
-          <Grid item xs={12} md={6} className={classes.imageContainer}>
-            <img src={country.flag} alt={`Flag of a ${country.name}`} className={classes.image} />
+    <Fade in>
+      <Container maxWidth="lg" className={classes.root}>
+        <Button
+          variant="contained"
+          color="secondary"
+          startIcon={<KeyboardBackspaceIcon />}
+          component={RouterLink}
+          to="/"
+        >
+          Back
+        </Button>
+        {country ? (
+          <Grid container className={classes.wrapper} alignItems="center">
+            <Grid item xs={12} md={6} className={classes.imageContainer}>
+              <img src={country.flag} alt={`Flag of a ${country.name}`} className={classes.image} />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Box className={classes.info}>
+                <Typography variant="h5" component="h2" className={classes.title}>
+                  {country.name}
+                </Typography>
+                <CountryDetails
+                  details={{
+                    nativeName: country.nativeName,
+                    population: country.population,
+                    region: country.region,
+                    subregion: country.subregion,
+                    capital: country.capital,
+                    topLevelDomain: country.topLevelDomain,
+                    currencies: country.currencies,
+                    languages: country.languages,
+                  }}
+                />
+                <Typography variant="h6" component="h3" className={classes.subtitle}>
+                  Border Countries:
+                </Typography>
+                <Grid container component="ul" className={classes.list} spacing={1}>
+                  <NeighborList neighbors={neighbors} />
+                </Grid>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <Box className={classes.info}>
-              <Typography variant="h5" component="h2" className={classes.title}>
-                {country.name}
-              </Typography>
-              <CountryDetails
-                details={{
-                  nativeName: country.nativeName,
-                  population: country.population,
-                  region: country.region,
-                  subregion: country.subregion,
-                  capital: country.capital,
-                  topLevelDomain: country.topLevelDomain,
-                  currencies: country.currencies,
-                  languages: country.languages,
-                }}
-              />
-              <Typography variant="h6" component="h3" className={classes.subtitle}>
-                Border Countries:
-              </Typography>
-              <Grid container component="ul" className={classes.list} spacing={1}>
-                <NeighborList neighbors={neighbors} />
-              </Grid>
-            </Box>
+        ) : (
+          <Grid item xs={12}>
+            <Typography
+              variant="h5"
+              component="h2"
+              display="block"
+              align="center"
+              className={classes.root}
+            >
+              No country found...
+            </Typography>
           </Grid>
-        </Grid>
-      ) : (
-        <Grid item xs={12}>
-          <Typography
-            variant="h5"
-            component="h2"
-            display="block"
-            align="center"
-            className={classes.root}
-          >
-            No country found...
-          </Typography>
-        </Grid>
-      )}
-    </Container>
+        )}
+      </Container>
+    </Fade>
   );
 };
